94a312558625fed5d47146e6693fe94a24e6835e
Whermerson19 <whermersonc@gmail.com>
2021-06-08 08:52:05 -0300

    header

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,12 +1,32 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-
-  justify-content: center;
-  align-items: center;
-
   background-color: ${(props) => props.theme.colors.background_primary};
 `;
 
-export const Title = styled.Text``;
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+
+  background-color: ${(props) => props.theme.colors.header};
+
+  justify-content: flex-end;
+
+  padding: ${RFValue(32)}px ${RFValue(24)}px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${(props) => props.theme.fonts.primary_400};
+
+  color: ${(props) => props.theme.colors.text};
+`;
